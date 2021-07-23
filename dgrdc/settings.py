f4e2c6280dbc83982b986b14da96ba33bfdedb78
Navindu Dananga <navindudananga123@gmail.com>
2021-07-23 19:18:43 +0530

    added accounts and clusters as apps and configured login redirect URL

--- dgrdc/settings.py
+++ dgrdc/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # own
+    'clusters',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,6 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGIN_REDIRECT_URL = "/"
