eaf8f9efdd29b1c617e3172ff8d280bf3e3f8cb1
Navindu Dananga <navindudananga123@gmail.com>
2021-07-23 19:19:56 +0530

    urls for accounts added

--- dgrdc/urls.py
+++ dgrdc/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+# accounts
+from accounts.views import registerPage
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/register/', registerPage)
 ]
