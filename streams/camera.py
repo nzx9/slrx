20c029847b19efed3ef6e893148d3453177b34f6
Navindu Dananga <navindudananga123@gmail.com>
2021-07-27 21:26:20 +0530

    get access to web cam

--- streams/camera.py
+++ streams/camera.py
@@ -1,1 +1,17 @@
 import cv2
+import numpy as np
+
+
+class WebCam:
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.video.release()
+
+    def capture(self):
+        success, image = self.video.read()
+
+        fliped = cv2.flip(image, 1)
+        ret, jpeg = cv2.imencode(".jpg", fliped)
+        return jpeg.tobytes()
--- streams/views.py
+++ streams/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http.response import StreamingHttpResponse
+from streams.camera import WebCam
 
 # Create your views here.
+
+
+def home(request):
+    return render(request, "index.html")
+
+
+def frames(camera):
+    while True:
+        frame = camera.capture()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+
+def videoFeed(request):
+    return StreamingHttpResponse(frames(WebCam()), content_type='multipart/x-mixed-replace; boundary=frame')
