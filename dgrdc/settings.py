108a7e35b096e4f9e774fa67745b822aa49148c3
Navindu Dananga <navindudananga123@gmail.com>
2021-07-31 19:00:15 +0530

    new updates in files and static files generated

--- dgrdc/settings.py
+++ dgrdc/settings.py
@@ -148,6 +148,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (str(BASE_DIR.joinpath('static')),)
 
 MEDIA_URL = '/media/'
 
--- streams/urls.py
+++ streams/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path, include
+from django.urls import path
 from streams import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home),
     # path('upload/', views.uploadStreams, name="stream_uploads"),
     path('sub/', views.sub, name="sub")
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
