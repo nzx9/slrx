8aa25268261275a809f54f416753ec977bedf541
Navindu Dananga <navindudananga123@gmail.com>
2021-08-10 23:22:47 +0530

    adding words from txt files configured

--- words/urls.py
+++ words/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.words_view, name="words_view"),
     path("new/", views.add_new_words, name="add_new_words"),
     path('delete/<str:pk>/', views.delete_word, name="delete_word"),
+    path("bulk/", views.bulk_upload, name="bulk_upload_words")
 ]
--- words/views.py
+++ words/views.py
@@ -1,4 +1,4 @@
-from django.db.models.query_utils import PathInfo
+from django.contrib.messages.api import success
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from words.models import Word
@@ -45,3 +45,61 @@ def delete_word(request, pk):
     word = Word.objects.get(id=pk)
     word.delete()
     return redirect('words_view')
+
+
+@login_required(login_url='/accounts/login/')
+def bulk_upload(request):
+    if request.method == "POST":
+        # form = UploadFileForm(request.POST, request.FILES)
+        # request.POST[]
+        byte_file = b''
+        for chunk in request.FILES.get('bulk-file').chunks():
+            byte_file += chunk
+        word_file = byte_file.decode('utf-8')
+        words_splited = word_file.split('\n')
+        word_list = []
+        for word in words_splited:
+            x = word.split(",")
+            if len(x) == 3 and len(x[0].strip()) > 0 and len(x[1].strip()) > 0 and len(x[2].strip()) > 0:
+                word_list.append(
+                    (x[0].strip().lower(), x[1].strip().lower(), x[2].strip().lower()))
+            elif len(x) == 2 and len(x[0].strip()) > 0 and len(x[1].strip()) > 0:
+                word_list.append(
+                    (x[0].strip().lower(), x[1].strip().lower(), None))
+            elif len(x) == 1 and len(x[0].strip()) > 0:
+                word_list.append((x[0].strip().lower(), None, None))
+            else:
+                continue
+
+        file_name = request.FILES.get('bulk-file').name
+        titles = word_list[0]
+
+        lang_checked = [0, 0, 0]
+
+        error = False
+        error_msg = None
+        success = False  # TODO
+        success_msg = None  # TODO
+
+        for i in range(3):
+            if titles[i] != None:
+                if titles[i] == "sinhala":
+                    lang_checked[0] += 1
+                elif titles[i] == "english":
+                    lang_checked[1] += 1
+                elif titles[i] == "singlish":
+                    lang_checked[2] += 1
+                else:
+                    error = True
+                    error_msg = "'{0}' cann't recognized in {1}. First line of the {1} can only have comma seperated 'sinhala', 'english' or singlish only (CASE DOESN'T MATTER)".format(
+                        titles[i], file_name)
+            else:
+                error = True
+                error_msg = "First line of the {0} can only have comma seperated 'sinhala', 'english' or singlish only (CASE DOESN'T MATTER). 'NULL' Provided".format(
+                    file_name)
+        if lang_checked[0] != 1 or lang_checked[1] != 1 or lang_checked[2] != 1:
+            error = True
+            error_msg = "First line of the {0} can only have comma seperated 'sinhala', 'english' or singlish only (CASE DOESN'T MATTER). ONE OR MORE LANGUAGES ARE MISSING and ONE USED MORE THAN ONCE".format(
+                file_name)
+
+    return render(request, "word_bulk_upload.html", {'file_name': file_name, 'titles': titles, 'word_list': word_list[1:], 'error': error, 'error_msg': error_msg})
