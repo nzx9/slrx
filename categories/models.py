12e8fdec8766a1a21c975b9030e05e5cf82dc86c
Navindu Dananga <navindudananga123@gmail.com>
2021-10-29 20:29:10 +0530

    Category model added to db

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.db.models.deletion import CASCADE
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.CharField(max_length=500)
+    word_count = models.IntegerField(default=0)
+    created_by = models.ForeignKey(User, on_delete=CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
