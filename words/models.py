59ba792a715f4298496dc54a551cbf433c16d4b8
Navindu Dananga <navindudananga123@gmail.com>
2021-10-30 16:29:13 +0530

    category field added to the word model

--- words/models.py
+++ words/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from categories.models import Category
+from django.db.models.deletion import CASCADE
 # Create your models here.
 
 
@@ -8,6 +9,7 @@ class Word(models.Model):
     in_english = models.CharField(max_length=50)
     in_singlish = models.CharField(max_length=50, null=True)
     recorde_count = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=CASCADE, null=True)
     created_by = models.IntegerField(null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
