66829c78e9d8ab3a17c4c06b4224fa6da1c655db
Navindu Dananga <navindudananga123@gmail.com>
2021-11-01 22:27:47 +0530

    New routes added

--- categories/urls.py
+++ categories/urls.py
@@ -3,5 +3,7 @@ from categories import views
 
 urlpatterns = [
     path('', views.category_view, name="category_view"),
-    path('create', views.create_category, name="create_category")
+    path('create', views.create_category, name="create_category"),
+    path('<str:category>', views.category_each_view, name="category_each"),
+    path('update/<int:pk>', views.update_category, name="update_category")
 ]
