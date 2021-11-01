b1d525a61a211a029db89a0b827b7ab5b7696a07
Navindu Dananga <navindudananga123@gmail.com>
2021-11-01 22:28:43 +0530

    New field added to track user edits in category model

--- categories/models.py
+++ categories/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.db.models.deletion import CASCADE
+from django.db.models.deletion import CASCADE, SET_NULL
 from django.contrib.auth.models import User
 # Create your models here.
 
@@ -9,5 +9,7 @@ class Category(models.Model):
     description = models.CharField(max_length=500)
     word_count = models.IntegerField(default=0)
     created_by = models.ForeignKey(User, on_delete=CASCADE)
+    last_edit_by = models.ForeignKey(
+        User, null=True, on_delete=SET_NULL, related_name="last_edit_by")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
