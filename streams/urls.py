a61fec1cfe26b5dff62602c3fedd6409f4839450
Navindu Dananga <navindudananga123@gmail.com>
2021-11-02 16:18:41 +0530

    NEW path added (streams/table -> streams_table_home)

--- streams/urls.py
+++ streams/urls.py
@@ -12,5 +12,6 @@ urlpatterns = [
     path('submit/<str:e_word>/', views.submit, name="submit"),
     path('verification', views.streams_verification, name="stream_verification"),
     path('get/<str:pk>', views.get_stream, name="get_stream"),
-    path('verify/<str:pk>', views.verify_stream, name="verify_stream")
+    path('verify/<str:pk>', views.verify_stream, name="verify_stream"),
+    path('table', views.streams_table_home, name="streams_table_home")
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
