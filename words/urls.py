84d2febbfa52f4d34bc4b7f1e6f542dcdcc1cec9
Navindu Dananga <navindudananga123@gmail.com>
2021-11-04 00:56:00 +0530

    Routes updated

--- words/urls.py
+++ words/urls.py
@@ -4,8 +4,9 @@ from words import views
 urlpatterns = [
     path('', views.words_view, name="words_view"),
     path("new/", views.add_new_words, name="add_new_words"),
-    path('delete/<str:pk>/', views.delete_word, name="delete_word"),
     path("bulk/", views.bulk_upload, name="bulk_upload_words"),
     path("bulk/upload/", views.add_bulk_words_to_db, name="add_to_db"),
-    path("view/<int:word_pk>", views.detailed_word_view, name="detailed_word_view")
+    path("view/<int:word_pk>", views.detailed_word_view, name="detailed_word_view"),
+    path("update/<int:pk>", views.update_word, name="update_word"),
+    path("delete/<int:pk>", views.delete_word, name="delete_word"),
 ]
