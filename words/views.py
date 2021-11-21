a2bf8a3c20b0e4af8b8963b4f7cb338a2416e601
Navindu Dananga <navindudananga123@gmail.com>
2021-11-21 20:29:41 +0530

    Bulk upload now support setting category (issue: #2)

--- words/views.py
+++ words/views.py
@@ -112,34 +112,35 @@ def bulk_upload(request):
 
         for word in words_splited:
             x = word.split(",")
-            if len(x) == 3 and len(x[0].strip()) > 0 and len(x[1].strip()) > 0 and len(x[2].strip()) > 0:
+            if len(x) == 4 and len(x[0].strip()) > 0 and len(x[1].strip()) > 0 and len(x[2].strip()) > 0 and len(x[3].strip()) > 0:
+                 word_list.append(
+                    (x[0].strip().lower(), x[1].strip().lower(), x[2].strip().lower(), x[3].strip().lower()))
+            elif len(x) == 3 and len(x[0].strip()) > 0 and len(x[1].strip()) > 0 and len(x[2].strip()) > 0:
                 word_list.append(
-                    (x[0].strip().lower(), x[1].strip().lower(), x[2].strip().lower()))
-
+                    (x[0].strip().lower(), x[1].strip().lower(), x[2].strip().lower(), None))
             elif len(x) == 2 and len(x[0].strip()) > 0 and len(x[1].strip()) > 0:
                 word_list.append(
-                    (x[0].strip().lower(), x[1].strip().lower(), None))
+                    (x[0].strip().lower(), x[1].strip().lower(), None, None))
             elif len(x) == 1 and len(x[0].strip()) > 0:
-                word_list.append((x[0].strip().lower(), None, None))
+                word_list.append((x[0].strip().lower(), None, None, None))
             else:
                 continue
 
         titles = word_list[0]
-
         word_dict = {}
         count = 0
         for word in word_list[1:]:
             word_dict[count] = {titles[0]: word[0],
-                                titles[1]: word[1], titles[2]: word[2]}
+                                titles[1]: word[1], titles[2]: word[2], titles[3]: word[3]}
             count += 1
 
         word_json = json.dumps(word_dict)
         lang_checked = [0, 0, 0]
-
+        category_checked = False
         error = False
         error_msg = None
 
-        for i in range(3):
+        for i in range(4):
             if titles[i] != None:
                 if titles[i] == "sinhala":
                     lang_checked[0] += 1
@@ -147,9 +148,11 @@ def bulk_upload(request):
                     lang_checked[1] += 1
                 elif titles[i] == "singlish":
                     lang_checked[2] += 1
+                elif titles[i] == "category":
+                    category_checked = True
                 else:
                     error = True
-                    error_msg = "'{0}' cann't recognized in {1}. First line of the {1} can only have comma seperated 'sinhala', 'english' or singlish only (CASE DOESN'T MATTER)".format(
+                    error_msg = "'{0}' can't recognized in {1}. First line of the {1} can only have comma seperated 'sinhala', 'english' or singlish only (CASE DOESN'T MATTER)".format(
                         titles[i], file_name)
             else:
                 error = True
@@ -159,7 +162,12 @@ def bulk_upload(request):
             error = True
             error_msg = "First line of the {0} can only have comma seperated 'sinhala', 'english' or singlish only (CASE DOESN'T MATTER). ONE OR MORE LANGUAGES ARE MISSING and ONE USED MORE THAN ONCE".format(
                 file_name)
-    return render(request, "word_bulk_upload.html", {'file_name': file_name, 'titles': titles, 'word_list': word_list[1:], 'error': error, 'error_msg': error_msg, "word_json": word_json})
+        if not category_checked:
+            error = True
+            error_msg = "Category field not found in {}".format(file_name)
+    categories = Category.objects.all().order_by('pk')
+
+    return render(request, "word_bulk_upload.html", {'file_name': file_name, 'titles': titles, 'word_list': word_list[1:], 'error': error, 'error_msg': error_msg, "word_json": word_json, 'categories': categories})
 
 
 @ login_required(login_url='/accounts/login/')
@@ -174,14 +182,14 @@ def add_bulk_words_to_db(request):
             for e in body_data:
                 if body_data[e]["sinhala"] != None and body_data[e]["sinhala"].strip() != "" and body_data[e]["english"] != None and body_data[e]["english"].strip() != "":
                     newWord = Word()
-                    newWord.in_sinhala = body_data[e]["sinhala"].strip(
-                    ).lower()
-                    newWord.in_english = body_data[e]["english"].strip(
-                    ).lower()
-                    newWord.in_singlish = body_data[e]["singlish"].strip(
-                    ).lower()
-                    newWord.created_by = request.user.id
+                    newWord.in_sinhala = body_data[e]["sinhala"].strip().lower()
+                    newWord.in_english = body_data[e]["english"].strip().lower()
+                    newWord.in_singlish = body_data[e]["singlish"].strip().lower()
+                    newWord.created_by = request.user
                     try:
+                        if body_data[e]["category"] != None: 
+                            category = Category.objects.get(name=body_data[e]["category"])
+                            newWord.category = category
                         newWord.save()
                         title = "All done..."
                         msg += "<p><i class='check circle green icon'></i>'({0},{1})' added to the database </p>".format(body_data[e]["sinhala"].strip(
@@ -195,9 +203,9 @@ def add_bulk_words_to_db(request):
                     title = "Completed with errors..."
                     msg += "<p><i class='times circle red icon'></i> '({},{})' sinhala or english field recived the 'None', sinhala and english fields can't be NULL</p>".format(
                         body_data[e]["sinhala"], body_data[e]["english"])
-        except:
-            title = "Not valid JSON..."
-            msg += "<p><i class='times circle red icon'></i> JSON can't parse recived data</p>"
+        except Exception as e:
+            title = "Errors Occured..."
+            msg += "<p><i class='times circle red icon'></i> {}</p>".format(e)
     else:
         msg += "<p><i class='times circle red icon'></i> Only POST requests are accepted</p>"
     return HttpResponse(json.dumps({"title": title, "msg": msg}), content_type='application/json')
