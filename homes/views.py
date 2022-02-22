f283c8ea550172bce7173c916b672a814541b7eb
Navindu Dananga <navindudananga123@gmail.com>
2022-02-22 18:13:34 +0530

    fixed zero division bug

--- homes/views.py
+++ homes/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from streams.models import Stream, User_Stream
 from words.models import Word
+
 # Create your views here.
 
 
 def home_view(request):
-    if(request.user.is_authenticated):
+    if request.user.is_authenticated:
         word_count = Word.objects.all().count()
         uss = User_Stream.objects.filter(userId=request.user)
 
@@ -14,9 +15,9 @@ def home_view(request):
         to_verify_count = 0
 
         for us in uss:
-            if(us.streamId.verified == True):
+            if us.streamId.verified == True:
                 accept_count += 1
-            elif(us.streamId.verified == False):
+            elif us.streamId.verified == False:
                 reject_count += 1
             else:
                 to_verify_count += 1
@@ -24,15 +25,22 @@ def home_view(request):
         done_count = uss.count() - reject_count
         not_done_count = word_count - done_count
 
-        percent = int((accept_count / word_count) * 100)
-        return render(request, "home.html", {
-            "done_count": done_count,
-            "not_done_count": not_done_count,
-            "percent": percent,
-            "accept_count": accept_count,
-            "reject_count": reject_count,
-            "to_verify_count": to_verify_count,
-        })
+        if word_count > 0:
+            percent = int((accept_count / word_count) * 100)
+        else:
+            percent = 0
+        return render(
+            request,
+            "home.html",
+            {
+                "done_count": done_count,
+                "not_done_count": not_done_count,
+                "percent": percent,
+                "accept_count": accept_count,
+                "reject_count": reject_count,
+                "to_verify_count": to_verify_count,
+            },
+        )
     else:
         return render(request, "home.html")
 
