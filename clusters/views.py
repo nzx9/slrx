9fdf7ccfc3be8858aa07f0c02cf4310ed5b841b0
nzx9 <navindudananga123@gmail.com>
2022-10-02 10:52:08 +0530

    fixed the cluster download error

--- clusters/views.py
+++ clusters/views.py
@@ -16,10 +16,14 @@ def clusters_view(request):
     verified_count = 0
     for us in uss:
         s = Stream.objects.get(pk=us.streamId.pk)
-        if(s.verified == True):
+        if s.verified == True:
             verified_count += 1
 
-    return render(request, "clusters.html", {"total_count": total_count, "verified_count": verified_count})
+    return render(
+        request,
+        "clusters.html",
+        {"total_count": total_count, "verified_count": verified_count},
+    )
 
 
 def download_blob(source_blob_name, destination_file_name):
@@ -34,32 +38,41 @@ def download_blob(source_blob_name, destination_file_name):
 
 
 def download_clusters(request, type):
-    if(type == "verified"):
-        streams = Stream.objects.filter(verified=True).order_by('-pk')
+    if type == "verified":
+        streams = Stream.objects.filter(verified=True).order_by("-pk")
         tail = "verified"
     else:
-        streams = Stream.objects.all().order_by('-pk')
+        streams = Stream.objects.all().order_by("-pk")
         tail = "all"
 
     dt = datetime.now()
-    file_name = "dslsl_dataset-v{}-{}".format(dt, tail)
+    file_name = "slrx_dataset-v{}-{}".format(datetime.timestamp(dt), tail)
     dir_name = "./media/tmp/{}".format(file_name)
+    print(dir_name)
     for s in streams:
         splited = s.pos_firebase.split("/")
-        p = '{}/{}'.format(dir_name, splited[1])
+        p = "{}/{}".format(dir_name, splited[1])
         # wsconsumer.send_info()
-        Path(p).mkdir(parents=True, exist_ok=True)
+        try:
+            Path(p).mkdir(parents=True, exist_ok=True)
+        except Exception as e:
+            print(e)
+            return HttpResponse(f"Error Occured:: {e}")
         download_blob(s.pos_firebase, "{}/{}".format(p, splited[2]))
     shutil.make_archive(
-        "./media/archives/{}".format(file_name), 'zip', root_dir="./media/tmp", base_dir=file_name)
+        "./media/archives/{}".format(file_name),
+        "zip",
+        root_dir="./media/tmp",
+        base_dir=file_name,
+    )
     zippath = "./media/archives/{}.zip".format(file_name)
     # Open the file for reading content
-    with open(zippath, 'rb') as zip:
+    with open(zippath, "rb") as zip:
         # Set the mime type
         mime_type, _ = mimetypes.guess_type(zippath)
         # Set the return value of the HttpResponse
         response = HttpResponse(zip, content_type=mime_type)
         # Set the HTTP header for sending to browser
-        response['Content-Disposition'] = "attachment; filename=%s.zip" % file_name
+        response["Content-Disposition"] = "attachment; filename=%s.zip" % file_name
         # Return the response value
         return response
